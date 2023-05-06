7c152a7f690a8b79e2755f878de4852c4850f5a3
Alex <naplle58@gmail.com>
2023-05-06 14:38:26 +0300

    add: multiple players decoding

--- socket/player.go
+++ socket/player.go
@@ -29,15 +29,31 @@ func encodePlayer(p elgo.Player) string {
 }
 
 func decodePlayer(s string) (elgo.Player, error) {
-	data := strings.SplitN(s, ";", 1)
-	if len(data) != 2 {
-		return nil, ErrBadInput
+	id, ratingStr, found := strings.Cut(s, ";")
+	if !found {
+		return nil, fmt.Errorf("%w: %s", ErrBadInput, s)
 	}
 
-	r, err := strconv.ParseFloat(data[1], 64)
+	r, err := strconv.ParseFloat(ratingStr, 64)
 	if err != nil {
 		return nil, fmt.Errorf("parse rating: %w", err)
 	}
 
-	return &SocketPlayer{ID: data[0], ELO: r}, nil
+	return &SocketPlayer{ID: id, ELO: r}, nil
+}
+
+func decodePlayers(s string) ([]elgo.Player, error) {
+	split := strings.Split(s, " ")
+	players := make([]elgo.Player, 0, len(split))
+
+	for _, playerStr := range split {
+		decoded, err := decodePlayer(playerStr)
+		if err != nil {
+			return nil, fmt.Errorf("decode: %w", err)
+		}
+
+		players = append(players, decoded)
+	}
+
+	return players, nil
 }
