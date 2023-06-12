9ad508e27f6ed9c7f114df1e790dfe7cb72f20c8
Alex <naplle58@gmail.com>
2023-06-13 01:07:25 +0300

    refactor: move calc to a separate package

--- calc/calculator.go
+++ calc/calculator.go
@@ -1,8 +1,10 @@
-package elgo
+package calc
 
 import (
 	"math"
 	"sort"
+
+	"github.com/ravsii/elgo"
 )
 
 type kFactor struct {
@@ -20,7 +22,7 @@ func (f kFactors) Less(i, j int) bool { return f[i].startsAt < f[j].startsAt }
 
 // Calculator holds options for calculations. Use
 //
-//	elgo.NewCalc(...)
+//	calc.New(...)
 //
 // to create a new calculator.
 type Calculator struct {
@@ -28,11 +30,11 @@ type Calculator struct {
 	kFactors kFactors
 }
 
-// NewCalc creates a new calculator for rating changes.
+// New creates a new calculator for rating changes.
 // k is used as a default option and you can pass multiple k factors that
 // will be applied depending on players' rating. Thus:
 //
-//	NewCalc(5, WithKFactor(1000, 20), WithKFactor(1500, 30))
+//	New(5, WithKFactor(1000, 20), WithKFactor(1500, 30))
 //
 // will be used as:
 //
@@ -40,9 +42,11 @@ type Calculator struct {
 //	rating >= 1000 && rating < 1500, k = 20
 //	rating >= 1500, k = 30
 //
+// K-factors are sorted automatically.
+//
 // There's no option to add max value for rating ranges,
 // it's either infinite or next factor's min value.
-func NewCalc(k float64, opts ...CalcOpt) *Calculator {
+func New(k float64, opts ...CalcOpt) *Calculator {
 	c := &Calculator{k: k}
 
 	for _, option := range opts {
@@ -54,35 +58,45 @@ func NewCalc(k float64, opts ...CalcOpt) *Calculator {
 	return c
 }
 
-// Win calculates rating for winner and loser.
-// The order of players in returned values is the same.
-func (c *Calculator) Win(winner, loser Ratinger) (float64, float64) {
+// Win calculates new rating for winner and loser.
+// If one if the players is nil, then `0, 0` is returned.
+func (c *Calculator) Win(winner, loser elgo.Ratinger) (winnerNew, loserNew float64) {
 	if winner == nil || loser == nil {
 		return 0, 0
 	}
 
-	winnerNew := winner.Rating() + c.getK(winner)*(1-probability(winner, loser))
-	loserNew := loser.Rating() + c.getK(loser)*(-probability(loser, winner))
+	return c.WinFloat(winner.Rating(), loser.Rating())
+}
+
+// WinFloat acts like Win() but accepts floats instead of interfaces.
+func (c *Calculator) WinFloat(winner, loser float64) (winnerNew, loserNew float64) {
+	winnerNew = winner + c.getK(winner)*(1-probability(winner, loser))
+	loserNew = loser + c.getK(loser)*(-probability(loser, winner))
 
 	return winnerNew, loserNew
 }
 
 // Draw calculates rating for both players using a 0.5 co-efficient.
-// The order of players in returned values is the same.
-func (c *Calculator) Draw(p1, p2 Ratinger) (float64, float64) {
-	if p1 == nil || p2 == nil {
+// If one if the players is nil, then `0, 0` is returned.
+func (c *Calculator) Draw(player1, player2 elgo.Ratinger) (player1New, player2New float64) {
+	if player1 == nil || player2 == nil {
 		return 0, 0
 	}
 
-	p1New := p1.Rating() + c.getK(p1)*(0.5-probability(p1, p2))
-	p2New := p2.Rating() + c.getK(p2)*(0.5-probability(p2, p1))
+	return c.DrawFloat(player1.Rating(), player2.Rating())
+}
+
+// DrawFloat acts like Draw() but accepts floats instead of interfaces.
+func (c *Calculator) DrawFloat(player1, player2 float64) (player1New, player2New float64) {
+	p1New := player1 + c.getK(player1)*(0.5-probability(player1, player2))
+	p2New := player2 + c.getK(player2)*(0.5-probability(player2, player1))
 
 	return p1New, p2New
 }
 
-func (c *Calculator) getK(r Ratinger) float64 {
+func (c *Calculator) getK(rating float64) float64 {
 	for _, bracket := range c.kFactors {
-		if bracket.startsAt >= r.Rating() {
+		if bracket.startsAt >= rating {
 			return bracket.k
 		}
 	}
@@ -92,7 +106,7 @@ func (c *Calculator) getK(r Ratinger) float64 {
 
 // probability counts probability of winning of a player against opponent
 // using this formula: https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
-func probability(player, opponent Ratinger) float64 {
+func probability(player, opponent float64) float64 {
 	// note: can't use Pow10 here because Rating() returns float64
-	return 1.0 / (1.0 + math.Pow(10.0, (opponent.Rating()-player.Rating())/400.0))
+	return 1.0 / (1.0 + math.Pow(10.0, (opponent-player)/400.0))
 }
